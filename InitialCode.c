aee9b6c6761ef1f3aff3a87ddf7c71a343dbd051
Sharan <sharanm087@gmail.com>
2020-05-21 10:56:24 -0500

    Update InitialCode.c

--- InitialCode.c
+++ InitialCode.c
@@ -53,13 +53,6 @@ int Validate_Pwd(char a[])
     return 1;
 }
 
-int stringlength(char s[])
-{
- int i;
-  for (i = 0; s[i] != '\0'; ++i);
-  return i;
-}
-
 /* Cunit testcases to validate the Pwd */
 void ValidatePwd_CUnitTest()
 {
